0c3ba4761b62dd098e8817849ff184c453fbd510
wjduand <794062172@qq.com>
2016-05-25 00:51:18 -0700

    Update diffdominion.sh

--- diffdominion.sh
+++ diffdominion.sh
@@ -4,17 +4,17 @@
 cd $1
 make clean
 make all
-./testdominion $3 >> results.out
+./testdominion $3 >> results1.out
 echo "GCOV After Playing a Game" >> results.out
-gcov dominion.c >> results1.out
+gcov dominion.c >> results10.out
 cp results1.out $2
 cd ..
 cd $2
 make clean
 make all
-./testdominion $3 >> results.out
+./testdominion $3 >> results2.out
 echo "GCOV After Playing a Game" >> results.out
-gcov dominion.c >> results2.out
+gcov dominion.c >> results20.out
 
 if diff -s -y results1.out results2.out; then
     echo "TEST PASSED"
